8912345985b84932e9323e718e62abd5536434a9
Abdullah <heighter79@gmail.com>
2023-08-19 04:18:44 -0700

    made the TO DO list app in oop

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def to_do_home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run()
